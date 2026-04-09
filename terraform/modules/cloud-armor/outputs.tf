b20f1a01c0f07bd62ed3cb5b95ff3a8f8b704314
Manu Patel <manu.patel@ingka.ikea.com>
2026-04-09 10:11:59 +0200

    chore: move to root, remove terraform and docker files

--- terraform/modules/cloud-armor/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "self_link" {
-  description = "Self-link of the Cloud Armor security policy."
-  value       = google_compute_security_policy.this.self_link
-}
-
-output "name" {
-  description = "Name of the Cloud Armor security policy."
-  value       = google_compute_security_policy.this.name
-}
--- terraform/modules/load-balancer/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "static_ip" {
-  description = "Global static IP address. Point your DNS A record here."
-  value       = google_compute_global_address.this.address
-}
